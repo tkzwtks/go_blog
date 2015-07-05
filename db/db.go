c37aaea43da33c8751813ba00f40e2d165eb023b
Takashi Takizawa <tkzwtks@gmail.com>
2015-07-05 13:11:29 +0900

    tmp

--- db/db.go
+++ db/db.go
@@ -1,12 +1,10 @@
 package db
 
 import (
-	"log"
 	"os"
 
 	_ "github.com/go-sql-driver/mysql"
 	"github.com/jinzhu/gorm"
-	"github.com/joho/godotenv"
 )
 
 type connection struct {
@@ -17,11 +15,12 @@ var conn *connection
 
 func GetConnection() gorm.DB {
 	if conn == nil {
-		err := godotenv.Load()
-		if err != nil {
-			log.Fatal("Error loading .env file")
-		}
-
+		/*
+			err := godotenv.Load()
+			if err != nil {
+				log.Fatal("Error loading .env file")
+			}
+		*/
 		db, _ := gorm.Open("mysql",
 			os.Getenv("DB_USER")+":"+os.Getenv("DB_PASS")+"@/"+os.Getenv("DB_NAME")+"?charset=utf8&parseTime=True&loc=Local")
 		conn = &connection{connection: db}
